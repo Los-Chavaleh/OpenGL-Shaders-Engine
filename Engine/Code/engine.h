9a3f029562d9c7b47983fe1afa6e6d03e114bbdb
RoperoIvan <ivan_ropero98@hotmail.com>
2021-04-17 18:39:51 +0200

    yessss patrisio

--- Engine/Code/engine.h
+++ Engine/Code/engine.h
@@ -14,6 +14,12 @@ typedef glm::ivec2 ivec2;
 typedef glm::ivec3 ivec3;
 typedef glm::ivec4 ivec4;
 
+struct VertexV3V2
+{
+    glm::vec3 pos;
+    glm::vec2 uv;
+};
+
 struct Image
 {
     void* pixels;
@@ -28,17 +34,49 @@ struct Texture
     std::string filepath;
 };
 
+struct VertexShaderAttribute
+{
+    u8 location;
+    u8 componentCount;
+};
+
+struct VertexShaderLayout
+{
+    std::vector<VertexShaderAttribute> attributes;
+};
+
+struct VertexBufferAttribute
+{
+    u8 location;
+    u8 componentCount;
+    u8 offset;
+};
+
+struct VertexBufferLayout
+{
+    std::vector<VertexBufferAttribute> attributes;
+    u8                                 stride;
+};
+
+struct Vao
+{
+    GLuint handle;
+    GLuint programHandle;
+};
+
 struct Program
 {
     GLuint             handle;
     std::string        filepath;
     std::string        programName;
     u64                lastWriteTimestamp; // What is this for?
+    VertexShaderLayout vertexInputLayout;
 };
 
 enum Mode
 {
     Mode_TexturedQuad,
+    Mode_Model,
     Mode_Count
 };
 
@@ -56,10 +94,40 @@ struct OpenGLInfo
     }
 };
 
-struct VertexV3V2
+struct Model
 {
-    glm::vec3 pos;
-    glm::vec2 uv;
+    u32 meshIdx;
+    std::vector<u32> materialIdx;
+};
+
+struct Submesh
+{
+    VertexBufferLayout vertexBufferLayout;
+    std::vector<float> vertices;
+    std::vector<u32> indices;
+    u32              vertexOffset;
+    u32              indexOffset;
+    std::vector<Vao> vaos;
+};
+
+struct Mesh
+{
+    std::vector<Submesh> submeshes;
+    GLuint               vertexBufferHandle;
+    GLuint               indexBufferHandle;
+};
+
+struct Material
+{
+    std::string name;
+    vec3        albedo;
+    vec3        emissive;
+    f32         smoothness;
+    u32         albedoTextureIdx;
+    u32         emissiveTextureIdx;
+    u32         specularTextureIdx;
+    u32         normalsTextureIdx;
+    u32         bumpTextureIdx;
 };
 
 struct App
@@ -78,18 +146,22 @@ struct App
     ivec2 displaySize;
 
     std::vector<Texture>  textures;
+    std::vector<Material>  materials;
+    std::vector<Mesh>  meshes;
+    std::vector<Model>  models;
     std::vector<Program>  programs;
 
     // program indices
     u32 texturedGeometryProgramIdx;
-    
+    u32 texturedMeshProgramIdx;
+    u32 meshProgramIdx;
     // texture indices
     u32 diceTexIdx;
     u32 whiteTexIdx;
     u32 blackTexIdx;
     u32 normalTexIdx;
     u32 magentaTexIdx;
-
+    u32 model;
     // Mode
     Mode mode;
 
@@ -100,10 +172,11 @@ struct App
 
     // Location of the texture uniform in the textured quad shader
     GLuint programUniformTexture;
-
+    GLuint texturedMeshProgram_uTexture;
     // VAO object to link our screen filling quad with our textured quad shader
     GLuint vao;
 
+    // GPU Info
     OpenGLInfo oglInfo;
 };
 
